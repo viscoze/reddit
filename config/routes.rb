b54044bfadaa6db2f5d268a7252890f8f446888e
Viscoze <vlad.oganov@gmail.com>
2016-05-01 16:33:24 +0300

    Add likes and dislikes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :links
   root to: 'links#index'
+  devise_for :users
+  resources :links do
+    member do
+      put "like",    to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+  end
 end
